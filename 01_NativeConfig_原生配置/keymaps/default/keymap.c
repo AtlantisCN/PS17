3c0961c1904e4ac3d837dc66749b1028e9ef1557
AtlantisCN <33477492+AtlantisCN@users.noreply.github.com>
2022-07-20 09:23:12 +0800

    Update keymap.c

--- 01_NativeConfig_原生配置/keymaps/default/keymap.c
+++ 01_NativeConfig_原生配置/keymaps/default/keymap.c
@@ -1,9 +1,25 @@
 #include "PS17.h"
 
+//added by xc 
+bool is_alt_tab_active = false; // Super Alt Tab Code
+uint16_t alt_tab_timer = 0;
+#ifdef VIA_ENABLE
+	enum custom_keycodes { //Use USER 00 instead of SAFE_RANGE for Via. VIA json must include the custom keycode.
+	  ATABF = USER00, //Alt tab forwards
+	  ATABR //Alt tab reverse
+	};
+#else
+	enum custom_keycodes { //Use USER 00 instead of SAFE_RANGE for Via. VIA json must include the custom keycode.
+	  ATABF = SAFE_RANGE, //Alt tab forwards
+	  ATABR //Alt tab reverse
+	};
+#endif
+//ended by xc
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	KEYMAP(
-		KC_WH_U, MO(1), KC_WH_D, 
+		KC_VOLD, MO(1), KC_VOLU, 
 		KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS, 
 		KC_P7, KC_P8, KC_P9, KC_PPLS, 
 		KC_P4, KC_P5, KC_P6, 
@@ -11,29 +27,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_P0, KC_PDOT),
 
 	KEYMAP(
-		KC_VOLU, KC_TRNS, KC_VOLD, 
+		KC_A, KC_TRNS, KC_B, 
 		RGB_TOG, RGB_MOD, RGB_VAD, RGB_VAI, 
 		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
 		KC_TRNS, KC_TRNS, KC_TRNS, 
 		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS),
-
-	KEYMAP(
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS),
-
-	KEYMAP(
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
 		KC_TRNS, KC_TRNS)
-
 };
 
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
@@ -48,20 +47,71 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
 void matrix_init_user(void) {
 }
 
-void matrix_scan_user(void) {
-}
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-	return true;
+//below added by xc replace above part 2
+void matrix_scan_user(void) { //run whenever user matrix is scanned
+  if (is_alt_tab_active) {
+    if (timer_elapsed(alt_tab_timer) > 1000) {
+      unregister_code(KC_LALT);
+      is_alt_tab_active = false;
+    }
+  }
 }
+//above added by xc part 2
+
+
+//below added by xc part 3
+bool process_record_user(uint16_t keycode, keyrecord_t *record) { //Actions to override existing key behaviours
+	switch (keycode) { //For super alt tab keycodes
+	case ATABF:	//Alt tab forwards
+	  if (record->event.pressed) {
+		if (!is_alt_tab_active) {
+		  is_alt_tab_active = true;
+		  register_code(KC_LALT);
+		}
+		alt_tab_timer = timer_read();
+		register_code(KC_TAB);
+	  } else {
+		unregister_code(KC_TAB);
+	  }
+	  break;
+	case ATABR:	//Alt tab reverse
+	  if (record->event.pressed) {
+		if (!is_alt_tab_active) {
+		  is_alt_tab_active = true;
+		  register_code(KC_LALT);
+		}
+		alt_tab_timer = timer_read();
+		register_code(KC_LSHIFT);
+		register_code(KC_TAB);
+	  } else {
+		unregister_code(KC_LSHIFT);
+		unregister_code(KC_TAB);
+	  }
+	  break;
+	}
+	return true;
+};
+//above by xc part 3
+
 
 void encoder_update_user(uint8_t index, bool clockwise) {
     if (index == 0) { /* First encoder */
         if (clockwise) {
-            tap_code(dynamic_keymap_get_keycode(biton32(layer_state), 0, 0));
-        } else {
-            tap_code(dynamic_keymap_get_keycode(biton32(layer_state), 0, 2));
-        }
+        	action_exec((keyevent_t){
+        					.key = (keypos_t){.row = 0, .col = 0}, .pressed = true, .time = (timer_read() | 1)  /* time should not be 0 */
+                       });  
+        	action_exec((keyevent_t){
+        					.key = (keypos_t){.row = 0, .col = 0}, .pressed = false, .time = (timer_read() | 1)  /* time should not be 0 */
+                       });  
+           } else {
+        	action_exec((keyevent_t){
+        					.key = (keypos_t){.row = 0, .col = 2}, .pressed = true, .time = (timer_read() | 1)  /* time should not be 0 */
+                       });
+        	action_exec((keyevent_t){
+        					.key = (keypos_t){.row = 0, .col = 2}, .pressed = false, .time = (timer_read() | 1)  /* time should not be 0 */
+                       });  
+           }
     }
 }
 
@@ -85,7 +135,6 @@ void led_set_user(uint8_t usb_led) {
 	} else {
 		DDRD &= ~(1 << 4); PORTD &= ~(1 << 4);
 	}
-
 	if (usb_led & (1 << USB_LED_COMPOSE)) {
 		
 	} else {
